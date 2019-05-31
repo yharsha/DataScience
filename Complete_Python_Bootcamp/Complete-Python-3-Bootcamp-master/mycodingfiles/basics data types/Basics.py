af929bf3de15509304fcece29e0e041b2f6f62d1
hyadala <hyadala@cisco.com>
2019-05-31 18:08:58 +0530

    datatypes

---
+++ Complete_Python_Bootcamp/Complete-Python-3-Bootcamp-master/mycodingfiles/basics data types/Basics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 19:13:26 2019
+
+@author: hyadala
+"""
+
+# \ - BACKSLASH is for escape sequence 
+print("\"Hello world\"")
+
+#refer to jupiter notebook also
