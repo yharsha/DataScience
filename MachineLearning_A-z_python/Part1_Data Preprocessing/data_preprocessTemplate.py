c741fa6b8e596659632a7a812d92b8e42143f16f
hyadala <hyadala@cisco.com>
2019-07-20 19:46:32 +0530

    python for ml and ds

--- MachineLearning_A-z_python/Part1_Data Preprocessing/data_preprocessTemplate.py
+++ MachineLearning_A-z_python/Part1_Data Preprocessing/data_preprocessTemplate.py
@@ -15,9 +15,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #import dataset
+#X-independent variable ,Y-Dep variable
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 print (X)
 Y = dataset.iloc[:,3].values
 print ('values of Y')
 print (Y)
+
+#Taking care of missing data using scikit library
+print('Taking care of missing data using scikit library')
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values=  'NaN' ,strategy="mean",axis=0)
+imputer=imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+print (X)
+
